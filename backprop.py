794eac593c3dbc9fe2c348c17db8202d86c1f166
Sandipan Nath <Sandipan Nath>
2020-10-04 22:22:29 +0530

    V3

--- backprop.py
+++ backprop.py
@@ -2,8 +2,8 @@ from sstate import State
 import sys
 
 def main():
-    print("Hello World")
-
-
-
-main()
+    inp = []
+    c = 0
+    for index in range(1, len(sys.argv)):
+        inp[c] = sys.argv[index]
+        print(type(inp[c]))
